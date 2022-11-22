ac8990241d166a8efd60011757f3e221eaec28c4
Yogesh Mane <yogeshmane008@gmail.com>
2022-11-22 16:27:47 +0530

    Java 22-11 Second commit

--- src/Program/Parctice_2211.java
+++ src/Program/Parctice_2211.java
@@ -1,10 +1,39 @@
 package Program;
 
+
 public class Parctice_2211 {
 
 	public static void main(String[] args) {
 		
 		System.out.println("HELLO THERE!!! I AM YOGESH MANE......");
+		
+		int a=10;
+		
+		int b=20;
+		
+		// Arithmetic Operators +,-,*,/,%
+		
+		System.out.println("Value of variable A is :" +a);
+
+		System.out.println("Value of variable B is :" +b);
+		
+		System.out.println("Addition of two variables :" +(a+b));
+		
+		System.out.println("Subtraction of two variables :" +(a-b));
+		
+		System.out.println("Multiplication of two variables :" +(a*b));
+		
+		System.out.println("Division of two variables :" +(a%b));
+		
+		
+		// Relational Operators <,>,<=,>=,==,=,!=
+		
+		System.out.println("Comparing two variables :" + (a<b));
+		
+		System.out.println("Comparing two variables :" + (a>b) );
+		
+		
+		//Logical Operators 
 		
 	}
 	
