56109767417026c23337200bf43f4f975880d0f9
Yogesh Mane <yogeshmane008@gmail.com>
2022-11-24 12:32:05 +0530

    24-11 Second Commit

---
+++ src/Program/reverseNumber.java
@@ -0,0 +1,21 @@
+package Program;
+
+import java.util.Scanner;
+
+public class reverseNumber {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter Number which you want to reverse :");
+		
+		Scanner s=new Scanner(System.in);
+		
+		int num=s.nextInt();
+		
+		System.out.println("Entered Number is :" +num);
+	
+		
+	}
+	
+	
+}
